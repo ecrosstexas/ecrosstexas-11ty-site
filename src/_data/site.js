de4a13bc8dadb9d30ac520720eda229f9a412779
Eric Wallace <eric@ecrosstexas.com>
2022-04-06 18:22:39 -0500

    lang and locale updates

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
 const environmentSpecificVariables = {
   development: {
-    url: 'http://localhost:8080',
+    url: 'http://localhost:8080'
   },
   production: {
-    url: 'https://www.ecrosstexas.com/',
-  },
+    url: 'https://www.ecrosstexas.com/'
+  }
 };
 
 module.exports = {
@@ -12,15 +12,24 @@ module.exports = {
   author: 'Eric Wallace',
   email: 'ecrosstexas@gmail.com',
   description: 'The personal website of Eric Wallace, a digital rancher in Plano, Texas.',
-  keywords: ['ecrosstexas', 'Eric Wallace', 'Christianity', 'PCA', 'presbyterian', 'reformed', 'Texas'],
-  language: 'en-US',
+  keywords: [
+    'ecrosstexas',
+    'Eric Wallace',
+    'Christianity',
+    'PCA',
+    'presbyterian',
+    'reformed',
+    'Texas'
+  ],
+  lang: 'en',
+  locale: 'en_us',
   repo: 'https://github.com/ecrosstexas/ecrosstexas-11ty-site',
   indieauth: true,
   indieauthAuth: 'https://indieauth.com/auth',
   indieauthToken: 'https://tokens.indieauth.com/token',
   favicon: {
     widths: [32, 57, 76, 96, 128, 192, 228],
-    format: 'png',
+    format: 'png'
   },
-  ...environmentSpecificVariables[process.env.ELEVENTY_ENV],
+  ...environmentSpecificVariables[process.env.ELEVENTY_ENV]
 };
